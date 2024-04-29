f66f659fb31e8a5944550f7b990839b678590391
Torngrund <153111084+Torngrund@users.noreply.github.com>
2024-04-29 14:55:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ window.fsAttributes = window.fsAttributes || [];
 window.fsAttributes.push([
   "cmsfilter",
   (listInstances) => {
-    // Select all the cities
+    // Select all the cities and their corresponding project names
     const cities = document.querySelectorAll('[wb-autocomplete="cities"]');
+    const projects = document.querySelectorAll('[wb-autocomplete="project"]');
     
-    // Initialize an empty array to store city names
+    // Initialize an empty array to store city names along with project names
     const uniqueCitiesArray = [];
     
-    // Iterate over cities and add their text content to the array
-    cities.forEach((city) => {
-      uniqueCitiesArray.push(city.textContent);
+    // Iterate over cities and add their text content along with project names to the array
+    cities.forEach((city, index) => {
+      const cityName = city.textContent;
+      const projectName = projects[index].textContent;
+      uniqueCitiesArray.push(`${cityName} - ${projectName}`);
     });
+
+    // Sort the array alphabetically
+    uniqueCitiesArray.sort();
 
     const autoCompleteJS = new autoComplete({
       selector: "#autoComplete",
